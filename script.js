f235630567d4e20e1ee9c261833640f84bf500a9
Dattatray_sul <suldattatray109@gmail.com>
2026-09-13 15:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+
+    square.addEventListener("mouseenter", () => {
+
+        squares.forEach((otherSquare) => {
+
+            if (otherSquare !== square) {
+                otherSquare.style.backgroundColor = "#6F4E37";
+            } else {
+                otherSquare.style.backgroundColor = "#E6E6FA";
+            }
+
+        });
+
+    });
+
+    square.addEventListener("mouseleave", () => {
+
+        squares.forEach((otherSquare) => {
+            otherSquare.style.backgroundColor = "#E6E6FA";
+        });
+
+    });
+
+});
